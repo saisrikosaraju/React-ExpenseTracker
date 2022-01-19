197e868caa5e6207c55175ab617bb1af61fe9cfb
Sai Sri kosaraju <koar.sail@gmail.com>
2022-01-19 11:57:10 +0100

    Update AllExpenses.js

--- src/components/Expenses/AllExpenses.js
+++ src/components/Expenses/AllExpenses.js
@@ -9,10 +9,12 @@ import "./AllExpenses.css";
 const  AllExpenses = (props)=>{
 
     const [yearFiltered,setYearFiltered] = useState("2021");
-
+      
     const changeHandler = yearSelected =>{
         setYearFiltered(yearSelected);
     }
+    
+    //expenses are filtered based on selected year
 
     const filteredExpenses = props.items.filter(expense =>{
         return expense.date.getFullYear().toString() === yearFiltered;
